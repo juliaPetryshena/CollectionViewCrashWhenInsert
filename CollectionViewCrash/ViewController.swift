b29b99ac8ad7813a19a9b35a06cf0b5bbc15dbcb
Petryshena, Julia <jpetryshena@marktplaats.nl>
2016-10-05 17:33:04 +0200

    CollectionView crashes if you trying to insert object at index 0

--- CollectionViewCrash/ViewController.swift
+++ CollectionViewCrash/ViewController.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UICollectionViewController {
+    var array:[Int] = []
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        collectionView?.dataSource = self
+        self.insert()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return array.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+    }
+    
+    func insert () {
+        array.append(1)
+        let currentIndex = array.count - 1
+        
+        if currentIndex  > 0 {
+            self.collectionView?.insertItems(at: [IndexPath(row:currentIndex, section:0)])
+        } else {
+            self.collectionView?.reloadData()
+        }
 
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            self.insert()
+        }
+    }
 }
-
